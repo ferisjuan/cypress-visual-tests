c58dbcbc9b39de8aa4602547d05de28dac0c6df5
juanferis <jferisgo@directvla.com.ar>
2020-11-03 15:13:20 -0500

    Override Time and Dates

--- cypress/integration/image.spec.js
+++ cypress/integration/image.spec.js
@@ -6,6 +6,8 @@ describe('Visual Regression', () => {
 	sizes.forEach(size => {
 		pages.forEach(page => {
 			it(`Should match ${page} in resolution ${size}`, () => {
+				const currentTime = new Date(Date.UTC(2020, 1, 1)).getDate(currentTime)
+				cy.clock()
 				cy.setResolution(size)
 				cy.visit(page)
 				cy.matchImageSnapshot()
